1c17a29d2f4a65042948a2ab575d1355376cb6c4
Isaac Irungu <lazerbeam1023@gmail.com>
2021-07-22 14:15:51 +0300

    7/22/2021

--- 17 - Reactive Forms/Angular/src/app/app.module.ts
+++ 17 - Reactive Forms/Angular/src/app/app.module.ts
@@ -1,19 +1,20 @@
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ContactFormComponent } from './contact-form/contact-form.component';
+import { SignupFormComponent } from './signup-form/signup-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContactFormComponent
+    SignupFormComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    ReactiveFormsModule,
     FormsModule
   ],
   providers: [],
